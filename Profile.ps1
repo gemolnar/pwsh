eb5f7e140bb4cdfce22f49d02b9d40e5f9e495b9
Gergo MOLNAR <gergo.molnar@abacusmedicine.com>
2023-10-12 10:43:21 +0200

    add predictors

--- Profile.ps1
+++ Profile.ps1
@@ -1,11 +1,13 @@
-
-Write-Host "Hello, pwsh!"
-
+#
 # The original pwsh profile file can be found under {my-documents}\PowerShell\Microsoft.PowerShell_profile.ps1
 # That file now does nothing but dot-sources this file.
+#
 
 oh-my-posh init pwsh --config "$env:USERPROFILE/src/pwsh/javelin.omp.json" | Invoke-Expression
 $env:POSH_AZURE_ENABLED = $true
+
+Import-Module Az.Tools.Predictor
+Import-Module CompletionPredictor
 
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
@@ -13,4 +15,7 @@ Set-PSReadLineOption -EditMode Windows
 
 $PSStyle.FileInfo.Directory = "`e[36m"
 
-Function gazc { Get-AzContext | Select-Object -Property Name,Subscription,Account | Format-List }
+
+function gazc {
+	Get-AzContext | Select-Object -Property Name,Subscription,Account | Format-List
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pwsh
-PowerShell related stuff
+Stuff I use to pimp up my PowerShell
+
+[PSReadLine](https://github.com/PowerShell/PSReadLine)
+[GraphicalTools](https://github.com/PowerShell/GraphicalTools)
+[Install-module -Name Az.Tools.Predictor][https://techcommunity.microsoft.com/t5/azure-tools-blog/announcing-general-availability-of-az-tools-predictor/ba-p/3297956]
+[Install-Module -Name CompletionPredictor](https://devblogs.microsoft.com/powershell/completion-predictor-version-0-1-1-release/)
+
