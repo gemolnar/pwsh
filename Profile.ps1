7a5f1f772e8e1d2c78bed2e836ced508e527a1f1
Gergo Molnar <gergo.molnar@abacusmedicine.com>
2024-07-12 08:27:19 +0200

    add posh-git

--- Profile.ps1
+++ Profile.ps1
@@ -3,18 +3,28 @@
 # That file now does nothing but dot-sources this file.
 #
 
+Write-Host "Initializing oh-my-posh..."
 oh-my-posh init pwsh --config "$env:USERPROFILE/src/pwsh/javelin.omp.json" | Invoke-Expression
 $env:POSH_AZURE_ENABLED = $true
 
+Write-Host "Importing module Az.Tools.Predictor..."
 Import-Module Az.Tools.Predictor
+
+Write-Host "Importing module CompletionPredictor..."
 Import-Module CompletionPredictor
 
+Write-Host "Setting PSReadLineOptions..."
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+#Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
 $PSStyle.FileInfo.Directory = "`e[36m"
 
+Write-Host "Importing module posh-git..."
+Import-Module posh-git
 
 function gazc {
 	Get-AzContext | Select-Object -Property Name,Subscription,Account | Format-List
